c661633ddef1c0872564fd6bb5fddfdf2fb1aa67
userproject <u.useremail@gmail.com>
2015-05-14 21:01:00 +0700

    fixed error

--- site/js/global.js
+++ site/js/global.js
@@ -16,6 +16,7 @@ function setheader() {
 	head_menu_h = $('.menu').height();
 	
 	$('#content').css({'padding-top': head_h+'px'});
+	$('.head_bg').css({'height': head_h+'px'});
 	$('.h_menu').css({'top': head_h+'px'});
 	$('.s_menu').css({'top': head_h+'px'});
 	
@@ -42,6 +43,7 @@ function setheader() {
 		}
 	} 	
 	
+	if($('body').hasClass('stopScroll')) {$('body').removeClass('stopScroll');}	
 	$('.s_menu').slideUp(250);
 	var catcount = $('.sh_sub').length;
 	for(var i = 0; i < catcount; i++) {
@@ -61,6 +63,7 @@ function headmenu() {
 			$('.h_btn').find('img').attr('src', 'images/share/img/h-menu-btn.png');
 		}
 		
+		$('body').removeClass('stopScroll');
 		$('.h_menu').slideUp(500);
 		menuFlag = false;
 	} else {
@@ -70,6 +73,7 @@ function headmenu() {
 			$('.h_btn').find('img').attr('src', 'images/share/img/h-x-btn.png');
 		}
 		
+		$('body').addClass('stopScroll');
 		$('.h_menu').slideDown(500);
 		menuFlag = true;
 	}
@@ -84,13 +88,7 @@ function submenu(num) {
 		} else if(subFlg == 'none') {
 			$('.s_menu').eq(num).stop().slideDown(250);
 		}
-	} else {
-		if(num == 0) {
-			window.location="product.html";
-		} else if (num == 1) {
-			window.location="testimonial.html";
-		}
-	}
+	} 
 }
 
 var matchpc =false;
@@ -125,21 +123,15 @@ $(document).ready(function(e) {
 	setTimeout(function() {
 		setScreen();
 		setheader();
-	}, 100);
+	}, 250);
     
-	var s_menu_H = 0;
-	var headH = 0;
 	$('.sh_sub').hover(
-		function() { 
-			s_menu_H = $(this).find('.s_menu').height() + 23;
-			
-			if($(window).width() > 790) {
-				$('#header').stop().animate({'height':head_h + s_menu_H + 'px'}, 250);
+		function() {
+			if(matchpc) {
 				$(this).find('.s_menu').stop().slideDown(300);
 			}
 		}, function() { 
-			if($(window).width() > 790) {
-				$('#header').stop().animate({'height':head_h+'px'}, 250);
+			if(matchpc) {
 				$(this).find('.s_menu').stop().slideUp(300);
 			}
 		}
@@ -159,7 +151,9 @@ $(document).ready(function(e) {
 
 $(window).resize(function(e) {
 	matchmedia();
-	setScreen();
-	setheader();
+	setTimeout(function() {
+		setScreen();
+		setheader();
+	}, 250);
 });
 
